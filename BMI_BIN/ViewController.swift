178ee2035a644ae89b7f724d1121247f7ab734f0
GyuBin Hwang <spiraldailydev@gmail.com>
2024-10-01 22:25:12 +0900

    add segue action

--- BMI_BIN/ViewController.swift
+++ BMI_BIN/ViewController.swift
@@ -7,10 +7,32 @@ class ViewController: UIViewController {
     @IBOutlet weak var weightTextField: UITextField!
     @IBOutlet weak var calculateButton: UIButton!
     
+    var bmi: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        initUI()
     }
 
+    func initUI(){
+        mainLabel.text = "키와 몸무게를 입력하세요"
+        calculateButton.layer.cornerRadius = 5
+        heightTextField.placeholder = "(CM)"
+        weightTextField.placeholder = "(KG)"
+    }
+    
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        print(#function)
+        return true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print(#function)
+        if segue.identifier == "toSecondVC" {
+            let secondVC = segue.destination as! SecondViewController
+            secondVC.modalPresentationStyle = .fullScreen
+            
+        }
+    }
 }
 
