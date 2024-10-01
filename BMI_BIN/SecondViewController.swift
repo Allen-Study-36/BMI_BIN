3f0501e2967ee625b3a369eeb0dadc13d28e14fb
GyuBin Hwang <spiraldailydev@gmail.com>
2024-10-01 22:42:18 +0900

    add caculate, data deliver logic

--- BMI_BIN/SecondViewController.swift
+++ BMI_BIN/SecondViewController.swift
@@ -9,12 +9,29 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
-    @IBOutlet weak var bmiNumResult: UIButton!
+    @IBOutlet weak var bmiNumResult: UILabel!
     @IBOutlet weak var bmiGradeResult: UILabel!
     @IBOutlet weak var backButton: UIButton!
     
+    var bmiNum: Double?
+    var bmiGrade: String?
+    var bmiGradeColor: UIColor?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        initUI()
     }
-  
+    
+    func initUI(){
+        bmiNumResult.text = "\(bmiNum ?? 0)"
+        bmiGradeResult.text = bmiGrade
+        bmiGradeResult.backgroundColor = bmiGradeColor
+        bmiNumResult.clipsToBounds = true
+        bmiNumResult.layer.cornerRadius = 8
+        backButton.layer.cornerRadius = 5
+    }
+    
+    @IBAction func backButtonTap(_ sender:UIButton){
+        self.dismiss(animated: true, completion: nil)
+    }
 }
--- BMI_BIN/ViewController.swift
+++ BMI_BIN/ViewController.swift
@@ -23,6 +23,11 @@ class ViewController: UIViewController {
     
     override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
         print(#function)
+        if heightTextField.text == "" || weightTextField.text == "" {
+            mainLabel.text = "키와 몸무게를 입력하셔야만 합니다!!!"
+            mainLabel.textColor = UIColor.red
+            return false
+        }
         return true
     }
     
@@ -32,6 +37,56 @@ class ViewController: UIViewController {
             let secondVC = segue.destination as! SecondViewController
             secondVC.modalPresentationStyle = .fullScreen
             
+            secondVC.bmiNum = bmi
+            secondVC.bmiGradeColor = getBackgroundColor()
+            secondVC.bmiGrade = getBMIAdviceString()
+        }
+    }
+    
+    func calculateBMI(height: String, weight: String) -> Double {
+        guard let h = Double(height), let w = Double(weight) else { return 0.0 }
+        var bmi = w / (h * h) * 10000
+        bmi = round(bmi * 10) / 10
+        return bmi
+    }
+    
+    @IBAction func calculateBMI(_ sender: UIButton){
+        bmi = calculateBMI(height: heightTextField.text ?? "", weight: weightTextField.text ?? "")
+    }
+    
+    func getBackgroundColor() -> UIColor {
+        guard let bmi = bmi else { return UIColor.black }
+        switch bmi {
+        case ..<18.6:
+            return UIColor(displayP3Red: 22/255, green: 231/255, blue: 207/255, alpha: 1)
+        case 18.6..<23.0:
+            return UIColor(displayP3Red: 212/255, green: 251/255, blue: 121/255, alpha: 1)
+        case 23.0..<25.0:
+            return UIColor(displayP3Red: 218/255, green: 127/255, blue: 163/255, alpha: 1)
+        case 25.0..<30.0:
+            return UIColor(displayP3Red: 255/255, green: 150/255, blue: 141/255, alpha: 1)
+        case 30.0...:
+            return UIColor(displayP3Red: 255/255, green: 100/255, blue: 78/255, alpha: 1)
+        default:
+            return UIColor.black
+        }
+    }
+    
+    func getBMIAdviceString() -> String {
+        guard let bmi = bmi else { return "" }
+        switch bmi {
+        case ..<18.6:
+            return "저체중"
+        case 18.6..<23.0:
+            return "표준"
+        case 23.0..<25.0:
+            return "과체중"
+        case 25.0..<30.0:
+            return "중도비만"
+        case 30.0...:
+            return "고도비만"
+        default:
+            return ""
         }
     }
 }
